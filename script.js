652576d310ddcdff596688652e7649a4067b758f
Tom Mileham <tom.mileham@hotmail.co.uk>
2022-03-07 11:21:23 +0000

    Feat Finished Desktop Layout and Added JS autoclose mobile menu

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const burgerBtn = document.getElementById("burger");
 const closeBtn = document.getElementById("close");
 const mobilenav = document.querySelector(".mobilenav");
+let mobileNavEnabled;
+
+window.addEventListener("resize", () => {
+  let windowWidth = window.innerWidth;
+
+  if (windowWidth > 767 && mobileNavEnabled) {
+    mobilenav.classList.toggle("mobile-nav-on");
+    mobilenav.classList.toggle("mobile-nav-off");
+    burgerBtn.classList.toggle("hidden");
+    closeBtn.classList.toggle("show");
+    mobileNavEnabled = false;
+  }
+});
 
 burgerBtn.addEventListener("click", () => {
   mobilenav.classList.toggle("mobile-nav-on");
   mobilenav.classList.toggle("mobile-nav-off");
   burgerBtn.classList.toggle("hidden");
   closeBtn.classList.toggle("show");
+  mobileNavEnabled = true;
 });
 
 closeBtn.addEventListener("click", () => {
@@ -14,6 +28,7 @@ closeBtn.addEventListener("click", () => {
   mobilenav.classList.toggle("mobile-nav-off");
   burgerBtn.classList.toggle("hidden");
   closeBtn.classList.toggle("show");
+  mobileNavEnabled = false;
 });
 
 console.log(burger);
